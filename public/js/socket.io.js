1c7f3cd0b1cdf6acba2707a1371e2aa99721bbfe
Linus Jansson <00Jansson@gmail.com>
2019-09-16 14:23:25 +0200

    made some stuff, uploaded via bat

--- public/js/socket.io.js
+++ public/js/socket.io.js
@@ -1,9 +1,19 @@
 var socket = io();
 
+isEditing = false;
+//Eventlistners
+$('#submitTask').on('click', addTask);
+
+//ReviceEvent
+
+
+//Functions
 function addTask(){
     let data = {};
-
     data.name = $('#taskNameInput').val();
     data.description = $('#taskDescriptionInput').val();
+    if(isEditing)
+        socket.emit('editTask', data);
+    else
     socket.emit('newTask', data);
 }
