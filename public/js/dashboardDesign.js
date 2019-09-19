46fde317e061d87d5b596f79cb05e89af6d74255
Linus Jansson <00Jansson@gmail.com>
2019-09-19 12:12:14 +0200

    made some stuff, uploaded via bat

--- public/js/dashboardDesign.js
+++ public/js/dashboardDesign.js
@@ -19,3 +19,19 @@ $('.card').hover(
         $(this).css("overflow", "hidden")
     }
 );
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
