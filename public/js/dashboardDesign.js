fd7e8b7e0f3fa3086c4719f20a027ae252c6a38c
Linus Jansson <00Jansson@gmail.com>
2019-09-19 11:39:18 +0200

    made some stuff, uploaded via bat

---
+++ public/js/dashboardDesign.js
@@ -0,0 +1,23 @@
+let taskView = $('#taskView');
+let spacer = $('.spacer');
+let tasks = $('.taskItem');
+
+
+// tasks.each(function (index) {
+//     tasks[index].on(() => {
+//         (this).addClass('itemHover');
+//         spacer.addClass('spacerSmall');
+//     }, () => {
+//         (this).removeClass('itemHover');
+//         spacer.removeClass('spacerSmall');
+//     });
+// });
+
+// $('.taskItem').hover(
+//     function () {
+//         $((this)).animate({ height: "5em" }, 100)
+//     }, function () {
+//         $((this)).animate({ height: "3em" }, 100)
+
+//     }
+// );
