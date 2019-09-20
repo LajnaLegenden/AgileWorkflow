af7b573d1d250a7d0c93d1bbc0ace9b9bb72d0d1
Mattias <mattias.00@live.se>
2019-09-20 12:16:41 +0200

    made some stuff, uploaded via bat

--- public/js/js.js
+++ public/js/js.js
@@ -3,7 +3,10 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
   $('.project').tooltip({ boundary: 'window' })
-  $(".project").click(function() {
+  $(".project").click(function()  {
       let id = $(this).attr("id");
       location.href = "/dashboard/" + id;
   })
+  $("#addProject").click(() => {
+
+  });
