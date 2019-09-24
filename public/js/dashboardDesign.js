c46d230c493dc95206408573c406774cb6a4ccb8
Mattias <mattias.00@live.se>
2019-09-24 11:59:27 +0200

    made some stuff, uploaded via bat

--- public/js/dashboardDesign.js
+++ public/js/dashboardDesign.js
@@ -8,7 +8,7 @@ function showScrollWhenHover(element){
     );
 }
 
-showScrollWhenHover(".card")
+showScrollWhenHover(".card ul")
 showScrollWhenHover("#allComments");
 showScrollWhenHover("#log");
 
